8711487d3e65930407dd3448bc95611c428ee6d7
dakhamohammed <isanagy@live.fr>
2023-06-23 01:53:52 -0700

    C function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - C function that multiplies two integer and return
+ * the result.
+ *
+ * @a: integer value
+ * @b: integer value
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -6,3 +6,6 @@
 2. 1-isdigit.c
   - C function that checks for a digit (0 through 9).
 
+3. 2-mul.c
+   - C function that multiplies two integers.
+
