0788b60fee615677874444a16328bfc4b75d1108
dakhamohammed <isanagy@live.fr>
2023-06-23 08:10:18 -0700

    C function that draws a diagonal line on the terminal.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * print_diagonal - C function that draws a diagonal line,
+ * on the terminal.
+ * @n: is the number of times the character \
+ * should be printed.
+ *
+ * Return: void, nothing.
+ */
 void print_diagonal(int n)
 {
 	int i;
