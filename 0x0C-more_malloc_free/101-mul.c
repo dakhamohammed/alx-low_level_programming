5d9d30f53b75bb8899222b604e1214292dfd1216
dakhamohammed <isanagy@live.fr>
2023-07-14 15:21:28 -0700

    Program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,16 +3,83 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void _is0(char *argv[]);
-char *_init_array(char *ar, int lar);
-int _checknumber(char *argv[], int n);
+/**
+ * _is_zero - function that check if any number is zero
+ * @argv: command line arguments.
+ *
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
+{
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
 
 /**
- * main - program that multiplies two positive numbers.
+ * _init_array - function that set memery to zero in a new array
+ * @ar: array of char.
+ * @lar: length of the array of char.
+ *
+ * Return: pointer to the array of char.
+ */
+char *_init_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _checknumber - function that check the length of the number
+ * and checks if number is in base 10.
+ * @argv: command line arguments.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknumber(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
+}
+
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
  * @argc: number of arguments.
  * @argv: command line arguments.
  *
- * Return: 0 - if (Success).
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
@@ -22,7 +89,7 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		printf("Error\n"), exit(98);
 	ln1 = _checknumber(argv, 1), ln2 = _checknumber(argv, 2);
-	_is0(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _init_array(nout, lnout);
@@ -56,75 +123,5 @@ int main(int argc, char *argv[])
 	}
 	printf("%s\n", nout);
 	return (0);
-}
-
-/**
- * _is0 - function that check if the given number is zero
- * @argv: command line arguments.
- *
- * Return: no return.
- */
-void _is0(char *argv[])
-{
-	int i, isn1 = 1, isn2 = 1;
-
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
-
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
-
-	if (isn1 == 1 || isn2 == 1)
-	{
-		printf("0\n");
-		exit(0);
-	}
-}
-
-/**
- * _init_array - function that set memory to zero in a new array
- * @ar: array of char.
- * @lar: length of the array of char.
- *
- * Return: pointer to array of char.
- */
-char *_init_array(char *ar, int lar)
-{
-	int i = 0;
-
-	for (i = 0; i < lar; i++)
-		ar[i] = '0';
-	ar[lar] = '\0';
-	return (ar);
-}
-
-/**
- * _checknumber - function that check the length of the number
- * and checks if number is in base 10.
- * @argv: command line arguments.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
-int _checknumber(char *argv[], int n)
-{
-	int ln;
-
-	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-	return (ln);
 }
 
