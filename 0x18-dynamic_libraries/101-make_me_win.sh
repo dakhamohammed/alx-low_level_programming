1f09d690aaa076ad181b0dba39f7f03b0be1c08a
dakhamohammed <isanagy@live.fr>
2023-09-18 17:19:49 -0700

    Giga Millions program.

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/dakhamohammed/alx-low_level_programming/raw/main/0x18-dynamic_libraries/inject.so
-export LD_PRELOAD=/tmp/inject.so ./gm 9 8 10 24 75 9
+LD_PRELOAD=/tmp/inject.so
