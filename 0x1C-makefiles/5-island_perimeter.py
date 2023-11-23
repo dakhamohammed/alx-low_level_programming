99977274c901474db096b918f504598dd0664ea9
dakhamohammed <isanagy@live.fr>
2023-11-22 18:48:11 -0800

    TODO: Task 5, technical interview.

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module that returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """method that returns the perimeter of island described in grid """
+    perimeter = 0
+    rows = len(grid)
+
+    if grid != []:
+        columns = len(grid[0])
+
+    # TODO: Continue next afternoon, ALLAH willing.
+
